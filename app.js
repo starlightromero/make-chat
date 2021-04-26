6d856573da3b73e6b11964c3b374ae81b7faf21f
Starlight Romero <starlightromero@protonmail.com>
2021-04-26 16:10:27 -0700

    [update] users can create new channels and join

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 //We'll store our online users here
 let onlineUsers = {};
-io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+//Save the channels in this object.
+let channels = {"General" : []}
 
+io.on("connection", (socket) => {
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+})
 
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());

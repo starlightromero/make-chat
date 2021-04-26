80da22556969ff2de53c881706337d991360072c
Starlight Romero <starlightromero@protonmail.com>
2021-04-26 16:13:49 -0700

    [update] implement changing channels

--- public/index.js
+++ public/index.js
@@ -1,12 +1,22 @@
 //index.js
 $(document).ready(()=>{
+
   const socket = io.connect();
+  
+  //Keep track of the current user
+  let currentUser;
   
   // Get the online users from the server
   socket.emit('get online users');
-
-  //Keep track of the current user
-  let currentUser;
+  
+  //Each user should be in the general channel by default.
+  socket.emit('user changed channel', "General");
+  
+  //Users can change the channel by clicking on its name.
+  $(document).on('click', '.channel', (e)=>{
+    let newChannel = e.target.textContent;
+    socket.emit('user changed channel', newChannel);
+  });
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
